91318287082a8a7e9dc0681f9d5932347613f3df
Gasshette <gesshokun@gmail.com>
2017-10-16 00:09:42 +0200

    Delete controller, using the auto generated one from Spring

--- src/main/java/com/grisel/repository/ItemRepository.java
+++ src/main/java/com/grisel/repository/ItemRepository.java
@@ -1,7 +1,6 @@
 package com.grisel.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.grisel.entity.Item;
